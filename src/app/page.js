66980a3ef286c3f699636cd01f17488729feea0c
unknown <2ndmurphy>
2024-05-10 22:21:21 -0700

    [update] page recommended and sticky navbar

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default async function Home() {
   const response1 = await fetch(`${baseurl}/top/anime?limit=12`)
   const topAnime = await response1.json() // popular anime
   
-  const animeId = 1 // ID of the anime
+  const animeId = 1 
   const response2 = await fetch(`${baseurl}/anime/${animeId}/recommendations?limit=12`)
   const newAnime = await response2.json() // Rekomendasi anime
 
