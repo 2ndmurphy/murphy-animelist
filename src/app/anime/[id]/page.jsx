7eb9f50893915e79d02e3848d177a3e63721861f
unknown <2ndmurphy>
2024-05-19 01:13:51 -0700

    [tweak] reusable api call & mobile responsive

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -11,9 +11,9 @@ const Page = async({ params: { id } }) => {
   const anime = await getAnimeResponse(`anime/${id}/full`)
   
   return (
-    <div className='flex flex-wrap md:flex-nowrap gap-2 px-6 w-full h-[450px] bg-color-primary bg-opacity-50'>
-      <section className='flex-1 h-full w-[800px]'>
-        <header aria-label="Breadcrumb" className='hidden md:block ml-[250px] mt-4 mb-6'>
+    <div className='flex flex-wrap gap-2 text-center md:px-6 w-full bg-color-primary bg-opacity-50'>
+      <section className='flex-1 w-full md:w-[800px]'>
+        <header aria-label="Breadcrumb" className='hidden md:block ml-[200px] mt-4 md:mb-3'>
           <ol className="flex items-center gap-1 text-sm text-gray-600">
             <li>
               <a href="#" className="block transition hover:text-gray-700">
@@ -76,22 +76,20 @@ const Page = async({ params: { id } }) => {
           </ol>
         </header>
         
-        <div className='flex flex-wrap md:flex-nowrap'>
+        <div className='flex flex-wrap md:flex-nowrap items-center justify-center text-center px-4'>
           <div className='rounded'>
-            <Image
+            <img 
               src={anime.data.images.webp.image_url}
-              alt={anime.data.images.jpg.image_url}
-              width={230}
-              height={230}
-              className='rounded object-cover'
+              alt={anime.data.images.jpg.image_url} 
+              className='w-40 h-[210px] md:w-[260px] md:h-[280px] mb-4 md:mb-8 mt-5'
             />
           </div>
           
-          <div className='block gap-4 mb-5 ml-5'>
-            <h1 className='text-3xl mb-8 text-color-primary'>{anime.data.title} - {anime.data.year}</h1>
+          <div className='block gap-4 mb-5 ml-5 md:text-left'>
+            <h1 className='text-2xl md:text-3xl mb-4 text-color-primary'>{anime.data.title} - {anime.data.year}</h1>
             
-            <div className='mb-8'>
-              <ul className='flex gap-2 text-color-secondary'>
+            <div className='mb-4'>
+              <ul className='flex gap-2 text-color-secondary justify-center md:justify-start text-sm'>
                 <li className='px-2 border rounded border-color-secondary'>HD</li>
                 <li className='px-2 border rounded border-color-secondary'>Sub Indo</li>
                 <li>•</li>
@@ -103,31 +101,38 @@ const Page = async({ params: { id } }) => {
               </ul>
             </div>
             
-            <div className='flex items-center relative mb-4'>
-              <button className='flex px-6 py-2 rounded-3xl bg-color-secondary text-color-primary'>
-                <span className='mr-2'><Play size={25} /></span>
+            <div className='flex items-center justify-center md:justify-start mb-4'>
+              <button className='flex px-10 py-4 md:px-6 md:py-2 rounded-3xl bg-color-secondary text-color-primary text-md md:text-lg'>
+                <span className='mr-2 text-lg'><Play size={25} /></span>
                 Watch Now
               </button>
             </div>
             
-            <div className='scrollbar overflow-y-auto text-sm w-[790px] h-[100px] overflow-hidden text-color-primary'>
+            <div className='hidden md:block scrollbar overflow-y-auto text-sm w-full md:w-[940px] h-[100px] overflow-hidden text-color-primary'>
               <p>{anime.data.synopsis}</p>
             </div>
-            <h3 className='text-color-secondary text-lg mt-3'>NONTON ANIME GRATIS HANYA DI MURPHYNIME!</h3>
-            
+            <h3 className='text-color-secondary text-md mt-3'>NONTON ANIME GRATIS HANYA BUAT SEMUA DI MURPHYNIME!</h3>
           </div>
         </div>
       </section>
       
-      <section className='w-[300px] py-20 px-4 h-full bg-color-dark bg-opacity-50'>
-        <div className='flex justify-center items-center'>
-          <ul className='text-color-primary text-lg'>
-            <li>Judul Alternatif: {anime.data.title_english}</li>
-            <li>Rating: {anime.data.rating}</li>
-            <li>MAL Score: {anime.data.score}</li>
-            <li>Aired: {anime.data.aired.prop.from.day} - {anime.data.aired.prop.from.month} - {anime.data.aired.prop.from.year}</li>
-            <li>Type: {anime.data.type}</li>
-            <li>Status: {anime.data.status}</li>
+      <section className='w-full md:w-[300px] md:py-6 md:px-4 px-4 py-4 bg-color-dark bg-opacity-50'>
+        <div className='flex justify-center items-center flex-col'>
+          <ul className='text-color-primary text-lg text-left w-full'>
+            <li className='mb-3 md:mb-4'>Judul Alternatif: {anime.data.title_english}</li>
+            <li className='mb-3 md:mb-4'>Rating: {anime.data.rating}</li>
+            <li className='mb-3 md:mb-4'>MAL Score: {anime.data.score}</li>
+            <li className='mb-3 md:mb-4'>Aired: {anime.data.aired.prop.from.day} - {anime.data.aired.prop.from.month} - {anime.data.aired.prop.from.year}</li>
+            <li className='mb-3 md:mb-4'>Type: {anime.data.type}</li>
+            <li className='mb-3 md:mb-4'>Status: {anime.data.status}</li>
+            <li className='w-full py-1 border-t border-b-2 border-t-color-primary border-b-color-primary'>
+              <div className='flex mb-2 justify-between'>
+                <h3>Genres :</h3>
+              </div>
+              <div className='md:hidden text-sm w-full h-[100px] overflow-y-auto scrollbar'>
+                <p>{anime.data.synopsis}</p>
+              </div>
+            </li>
           </ul>
         </div>
       </section>
